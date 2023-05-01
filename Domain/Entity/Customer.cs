1a854aedc634a56707cc7c91f4d04dcdf945d312
Alexandr M <alexmoshan27@gmail.com>
2023-05-01 16:11:30 +0300

    Add_Remained Entities

---
+++ Domain/Entity/Customer.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Abstract;
+
+namespace Domain.Entity;
+
+public class Customer : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public List<ShoppingCart> ShoppingCarts { get; set; }
+    public IEnumerable<Order> Orders { get; set; }
+}
+
+//User -> customer. logging -> check if cart exists. if not - Create
+//    item+ 
+//        CartItem +
+//        bill. ->after bill -> cart = null.
