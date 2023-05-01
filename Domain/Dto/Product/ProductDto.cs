b89b78a16600bbe31f5321ad4292f003cebe93f4
pepegunj <pepegunj@gmail.com>
2023-05-01 16:11:37 +0300

    added_product_dtos

---
+++ Domain/Dto/Product/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Dto.Product;
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public string Manufacturer { get; set; }
+    public double Price { get; set; }
+    public string Category { get; set; }
+    public string IconURL { get; set; }
+}
