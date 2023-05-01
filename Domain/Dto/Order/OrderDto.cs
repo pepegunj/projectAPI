952877732c69fd81ced175f922a8a6610c87660d
pepegunj <pepegunj@gmail.com>
2023-05-01 16:11:15 +0300

    added_order_dto

---
+++ Domain/Dto/Order/OrderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Dto.Order;
+
+public class OrderDto
+{
+    public double Price { get; set; }
+    public int UserId { get; set; }
+    public int ShoppingCartId { get; set; }
+    public string Adress { get; set; }
+    /*public virtual User User { get; set; }
+    public virtual ShoppingCartDto ShoppingCart { get; set; }*/
+}
