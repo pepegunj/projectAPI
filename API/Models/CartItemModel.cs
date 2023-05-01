bb3f93dbe0ad1da3a8bc4b76029e9f64cff9bed3
Alexandr M <alexmoshan27@gmail.com>
2023-05-01 17:21:33 +0300

    Added_Models for API

---
+++ API/Models/CartItemModel.cs
@@ -0,0 +1,17 @@
+﻿using API.Models.Abstract;
+
+namespace API.Models;
+
+public class CartItemModel : BaseModel
+{
+    public int Quantity { get; set; }
+    public int ProductId { get; set; }
+    public int CustomerId { get; set; }
+
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+
+    public string Category { get; set; }
+    public string iconURL { get; set; }
+    public int Price { get; set; }
+}
---
+++ API/Models/CustomerModel.cs
@@ -0,0 +1,10 @@
+﻿using API.Models.Abstract;
+
+namespace API.Models;
+
+public class CustomerModel : BaseModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int UserId { get; set; }
+}
