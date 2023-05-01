5afe48a0e6bd19a8b6c0fa4e43da87d348038aa6
Alexandr M <alexmoshan27@gmail.com>
2023-05-01 17:23:49 +0300

    added Exception Middleware

---
+++ API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using API.Models;
+
+namespace API.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly ILogger _logger;
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next, ILoggerFactory logger)
+    {
+        _logger = logger.CreateLogger<ExceptionMiddleware>();
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Something went wrong: {ex}");
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        await context.Response.WriteAsync(new ErrorDetails
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = exception.Message
+        }.ToString());
+    }
+}
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionMiddleware>();
+    }
+}
