2ad815fef485086f9893815f642c4a5b93013a07
Alexandr M <alexmoshan27@gmail.com>
2023-05-01 16:31:38 +0300

    configure implementations of generic repository

---
+++ Data/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Data.Repositories.Abstract;
+using Data.Repositories.Abstract.Base;
+using Domain.Entity;
+
+namespace Data.Repositories.Implementation;
+
+public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+{
+    public CategoryRepository(ApplicationDbContext db) : base(db)
+    {
+    }
+}
---
+++ Data/Repositories/Implementation/ShoppingCartRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+using Data.Repositories.Abstract;
+using Data.Repositories.Abstract.Base;
+using Domain;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories.Implementation;
+
+public class ShoppingCartRepository : BaseRepository<ShoppingCart>, IShoppingCartRepository
+{
+    public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+    {
+    }
+
+    public async Task BillCartByIdAsync(int id)
+    {
+        var cartFromDb = await GetByIdAsync(id);
+        cartFromDb.CartState = CartStates.Billed;
+        await UpdateAsync(cartFromDb);
+    }
+
+    public async Task<IEnumerable<ShoppingCart>> GetByCustomerIdAsync(Expression<Func<ShoppingCart, bool>> predicate)
+    {
+        return await _db.ShoppingCarts.Include(x => x.CartItems).ThenInclude(x => x.Product).Include(x => x.Customer)
+            .Where(predicate)
+            .ToListAsync();
+    }
+}
