7506119e374fdcbff505ead4faa29b99cb1e5eae
Alexandr M <alexmoshan27@gmail.com>
2023-05-01 16:09:57 +0300

    Add_CartItem Entity

---
+++ Domain/Entity/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Abstract;
+
+namespace Domain.Entity;
+
+public class CartItem : BaseEntity
+{
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+
+    public int ShoppingCartId { get; set; }
+    public ShoppingCart ShoppingCart { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+}
