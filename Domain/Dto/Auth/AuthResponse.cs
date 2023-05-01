928d89d590c2a1119baed4065d57a8c20c66c594
pepegunj <pepegunj@gmail.com>
2023-05-01 16:16:16 +0300

    fix response

--- Domain/Dto/Auth/AuthResponse.cs
+++ Domain/Dto/Auth/AuthResponse.cs
@@ -1,17 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-using static Domain.Constants;
+﻿namespace Domain.Dto.Auth;
 
-namespace Domain.Dto.Auth;
-
-public class AuthRequest
+public class AuthResponse
 {
-    [Required]
-    [MinLength(USERNAME_MIN_LENGTH)]
-    [MaxLength(USERNAME_MAX_LENGTH)]
-    public string UserName { get; set; }
-
-    [Required]
-    [MinLength(PASSWORD_MIN_LENGTH)]
-    [MaxLength(PASSWORD_MAX_LENGTH)]
-    public string Password { get; set; }
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Role { get; set; }
+    public string Token { get; set; }
 }
