d75c15b61c0a7aa52bcd7307aa93f1552a799d44
pepegunj <pepegunj@gmail.com>
2023-05-01 16:10:42 +0300

    added_auth_entities

---
+++ Domain/Dto/Auth/AuthRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Domain.Constants;
+
+namespace Domain.Dto.Auth;
+
+public class AuthRequest
+{
+    [Required]
+    [MinLength(USERNAME_MIN_LENGTH)]
+    [MaxLength(USERNAME_MAX_LENGTH)]
+    public string UserName { get; set; }
+
+    [Required]
+    [MinLength(PASSWORD_MIN_LENGTH)]
+    [MaxLength(PASSWORD_MAX_LENGTH)]
+    public string Password { get; set; }
+}
---
+++ Domain/Dto/Auth/RegistrationModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Domain.Constants;
+
+namespace Domain.Dto.Auth;
+
+public class RegistrationModel
+{
+    [MinLength(USERNAME_MIN_LENGTH)]
+    [MaxLength(USERNAME_MAX_LENGTH)]
+    public string UserName { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    [MinLength(PASSWORD_MIN_LENGTH)]
+    [MaxLength(PASSWORD_MAX_LENGTH)]
+    public string Password { get; set; }
+
+    [MinLength(PASSWORD_MIN_LENGTH)]
+    [MaxLength(PASSWORD_MAX_LENGTH)]
+    public string ConfirmationPassword { get; set; }
+
+    public string? ProfileImageURL { get; set; }
+}
